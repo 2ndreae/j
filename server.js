23bf61d77c1a87126878351e8ba58bb842c93409
2ndreae <166828273+2ndreae@users.noreply.github.com>
2025-05-07 14:38:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -71,6 +71,9 @@ wss.on("connection", (ws) => {
     });
 });
 
-server.listen(8000, () => {
-    console.log("Server is running on http://localhost:8000");
+const PORT = process.env.PORT || 8000;  
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
